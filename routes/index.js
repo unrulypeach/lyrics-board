ee73797c96a459a4fcbca075f03c805701fb03fc
LC <gingeraleisthebest@gmail.com>
2023-10-12 18:18:47 -0400

    added new item page

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,13 @@ const messages = [
     text: `Like no other you can't be replaced`,
     artist: 'Beach House',
     user: 'fruit',
-    added: new Date().toLocaleDateString(),
+    added: new Date(),
   },
   {
     text: `Don't let your silly dreams Fall in between The crack of the bed and the wall`,
     artist: 'MMJ',
     user: 'baste',
-    added: new Date().toLocaleDateString(),
+    added: new Date(),
   }
 ]
 /* GET home page. */
@@ -22,5 +22,11 @@ router.get('/', function(req, res, next) {
 
 router.get('/new', function(req, res, next) {
   res.render('form', {title: 'Your fave lyrics'})
+});
+
+router.post('/new', function(req, res, next){
+  const { text, artist, user } = req.body;
+  messages.unshift({text, artist, user, added: new Date()})
+  res.redirect('/');
 })
 module.exports = router;
